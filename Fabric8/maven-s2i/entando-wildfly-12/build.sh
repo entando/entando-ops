cd621d26d54e9d2e0941cc38272a74546a10db43
Ampie Barnard <ampie.private@gmail.com>
2018-06-21 16:33:00 +0200

    EN-1541 dropped Fabric8/Maven support

--- Fabric8/maven-s2i/entando-wildfly-12/build.sh
+++
@@ -1,4 +0,0 @@
-export VERSION=${1:-5.0.0}
-echo $VERSION
-#docker build --build-arg ENTANDO_VERSION=$VERSION -t entando/entando-fabric8s2i-wildfly-12:$VERSION .
-docker build -t entando/entando-fabric8s2i-wildfly-12:$VERSION .
