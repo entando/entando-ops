0e410dd55c0305185dd9196bff97ca354dcf8234
Ampie Barnard <ampie.private@gmail.com>
2018-08-07 17:34:08 +0200

    EN_1779 added build arg for ENTANDO_VERSION

--- Docker/development/base-images/entando-wildfly12-quickstart-docker/build.sh
+++ Docker/development/base-images/entando-wildfly12-quickstart-docker/build.sh
@@ -1,6 +1,6 @@
-export VERSION=${1:-5.0.1}
+export VERSION=${1:-5.0.1-SNAPSHOT}
 echo $VERSION
-docker build -t entando/entando-wildfly12-quickstart-docker:$VERSION \
+docker build --build-arg ENTANDO_VERSION=$VERSION -t entando/entando-wildfly12-quickstart-docker:$VERSION \
     -t 172.30.1.1:5000/entando/entando-wildfly12-quickstart-docker:$VERSION \
      -t entando/entando-wildfly12-quickstart-docker:latest  .
 
