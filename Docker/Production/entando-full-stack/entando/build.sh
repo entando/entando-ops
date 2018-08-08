66278a5278e3fdd3bcb0967c0314c65240d98792
Ampie Barnard <ampie.private@gmail.com>
2018-08-08 10:28:17 +0200

    EN-1779 introduced buildArg for the parent image

--- Docker/Production/entando-full-stack/entando/build.sh
+++ Docker/Production/entando-full-stack/entando/build.sh
@@ -1,3 +1,3 @@
-export VERSION=${1:-5.0.1}
+export VERSION=${1:-5.0.1-SNAPSHOT}
 echo $VERSION
-docker build -t entando/engine-api:$VERSION .
+docker build --build-arg ENTANDO_VERSION_TO_BUILD=$VERSION -t entando/engine-api:$VERSION .
