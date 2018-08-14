2b01507eb6579304c792bea71c980cb7d03d9fec
Ampie Barnard <ampie.private@gmail.com>
2018-08-14 07:31:56 +0200

    EN-1847 consolidated AppBuilder

---
+++ Docker/Production/entando-full-stack/appbuilder/build.sh
@@ -0,0 +1,7 @@
+export VERSION=${1:-5.0.1-SNAPSHOT}
+echo $VERSION
+export DOCKER_TAG=$VERSION
+export IMAGE_NAME="entando/app-builder-openshift:$VERSION"
+export DOCKERFILE_PATH="$(dirname $0)/Dockerfile"
+source hooks/build
+#docker push 172.30.1.1:5000/entando/app-builder-openshift:$VERSION
