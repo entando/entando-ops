d0b55f3e83f3085a67b2f8b2b2db82d601703c38
Ampie Barnard <ampie.private@gmail.com>
2018-08-14 12:07:24 +0200

    EN-1847 Ported microengine to new build technique

--- Docker/Production/entando-full-stack/microengine/start.sh
+++ Docker/Production/entando-full-stack/microengine/start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # Install and build entando app
-npm install && npm run build
-
+#npm run build
+pushd static/js
+#escape slashes
+ESCAPED_DOMAIN=$(echo $DOMAIN | sed -E "s/\//\\\\\//g")
+sed -i -E "s/$DOMAIN_MARKER/$ESCAPED_DOMAIN/g" $(ls main.*.js)
+if [ $USE_MOCKS=="true" ]; then
+     sed -i -E "s/USE_MOCKS\\:\\!1/USE_MOCKS\\:\\!0/g" $(ls main.*.js)
+fi
 #Execute serve
-serve -l 3000 -s build
+popd
+serve
