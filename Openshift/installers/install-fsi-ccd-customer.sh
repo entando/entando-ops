4af8629dc273e1beb4ff7a3ac40764d45a9cfc20
Ampie Barnard <ampie.private@gmail.com>
2018-09-13 06:59:01 +0200

    EN-1898 added UTF-8 as default encoding to support internationalized builds

--- Openshift/installers/install-fsi-ccd-customer.sh
+++ Openshift/installers/install-fsi-ccd-customer.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
-source $(dirname $0)/common.sh
+export ENTANDO_OPS_HOME="https://raw.githubusercontent.com/entando/entando-ops/EN-1928"
+
+function get_openshift_subdomain(){
+  if [ -x $(command  -v minishift) ]; then
+    PUBLIC_HOSTNAME=$(minishift config get public-hostname)
+    if [ $PUBLIC_HOSTNAME == "<nil>" ]; then
+      PUBLIC_HOSTNAME=$(minishift openshift config view | grep -oP "(?<=  subdomain: )[0-9\.a-zA-Z_\-]+")
+      if [ -z $PUBLIC_HOSTNAME ]; then
+        echo "$(minishift ip).nip.io"
+      else
+        echo $PUBLIC_HOSTNAME
+      fi
+    else
+      echo $PUBLIC_HOSTNAME
+    fi
+  elif [ -f  /etc/origin/master/master-config.yaml  ]; then
+    PUBLIC_HOSTNAME=$(sudo cat /etc/origin/master/master-config.yaml | grep -oP "(?<=  subdomain: )[0-9\.a-zA-Z_\-]+")
+    if [[ -z $PUBLIC_HOSTNAME ]]; then
+      echo "$(hostname -i).nip.io"
+    else
+      echo $PUBLIC_HOSTNAME
+    fi
+  else
+     >&2 echo "Neither Minishift nor the Openshift master-config.yml found. Are you running from a node with an Openshift installation?"
+     echo "COULD_NOT_RESOLVE_OS_SUBDOMAIN"
+  fi
+}
 echo "This script installs the Entando Sample project on the EAP 7.1 QuickStart image with a persistent embedded Derby database"
 oc replace --force -f $ENTANDO_OPS_HOME/Openshift/image-streams/entando-eap71-quickstart-openshift.json
 oc replace --force -f $ENTANDO_OPS_HOME/Openshift/image-streams/appbuilder.json
