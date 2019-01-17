08c3472c666fc0f6f132e28f20a55511ea01a803
Ampie Barnard <a.barnard@entando.com>
2019-01-17 06:57:06 +0200

    Inserted retry logic when Jetty throws ConcurrentModificationException

--- Docker/Production/entando-full-stack/push-all.sh
+++ Docker/Production/entando-full-stack/push-all.sh
@@ -1,1 +1,7 @@
-../../../common/push-all.sh
+#!/usr/bin/env bash
+echo -n "Waiting for Entando Engine ..."
+while ! [ $(docker inspect $(docker ps -q  -f name=entando-full-stack_engine-api*) --format="{{.State.Health.Status}}") = "healthy" ] ; do
+    echo -n "."
+    sleep 2
+done
+echo
