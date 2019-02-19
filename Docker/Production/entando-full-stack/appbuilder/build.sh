cfa4b2e954c7309d31eccf5dda4eaf06993a790d
Ampie Barnard <a.barnard@entando.com>
2019-02-19 17:09:24 +0200

    Added Digital Exchange capable AppBuilder image

--- Docker/Production/entando-full-stack/appbuilder/build.sh
+++ Docker/Production/entando-full-stack/appbuilder/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 export VERSION=${1:-5.0.3-SNAPSHOT}
 export ENTANDO_IMAGE="appbuilder"
-
+export DIGITAL_EXCHANGE_UI_ENABLED=false
+source ../../../../common/build-common.sh
+export ENTANDO_IMAGE="appbuilder-with-digital-exchange"
+export DIGITAL_EXCHANGE_UI_ENABLED=true
 source ../../../../common/build-common.sh
--- Docker/Production/entando-full-stack/appbuilder/start.sh
+++ Docker/Production/entando-full-stack/appbuilder/start.sh
@@ -8,9 +8,6 @@ ESCAPED_DOMAIN=$(echo $DOMAIN | sed -E "s/\//\\\\\//g")
 sed -i -E "s/$DOMAIN_MARKER/$ESCAPED_DOMAIN/g" $(ls main.*.js)
 ESCAPED_CLIENT_SECRET=$(echo $CLIENT_SECRET | sed -E "s/\//\\\\\//g")
 sed -i -E "s/$CLIENT_SECRET_MARKER/$ESCAPED_CLIENT_SECRET/g" $(ls main.*.js)
-if [ $USE_MOCKS=="true" ]; then
-     sed -i -E "s/USE_MOCKS\\:\\!1/USE_MOCKS\\:\\!0/g" $(ls main.*.js)
-fi
 #Execute serve
 popd
 serve
