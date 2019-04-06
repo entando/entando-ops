9103e65b8cd530b1729139a3a77140940e3eeaf1
Ampie Barnard <a.barnard@entando.com>
2019-04-06 21:36:27 +0200

    Made 5.2.0-SNAPSHOT the master branch

--- Docker/dependency-images/entando-dependencies-base/entando-common/determine-branch.sh
+++ Docker/dependency-images/entando-dependencies-base/entando-common/determine-branch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #NB!!! This file is copied from common/s2i in the Docker build hook. Only modify the original file!
 #Returns the appropriate branch in Entando's maven projects
-ENTANDO_MASTER_BRANCH_VERSION="5.1.0-SNAPSHOT"
+ENTANDO_MASTER_BRANCH_VERSION="5.2.0-SNAPSHOT"
 BRANCH=${ENTANDO_VERSION%-*}
 case "$ENTANDO_VERSION" in
     $ENTANDO_MASTER_BRANCH_VERSION )
